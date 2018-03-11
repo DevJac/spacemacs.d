a813ca206b596aca047ac0c6d0b6b10a0ee8883c
Devin Jacobs <dbuttonz@gmail.com>
2018-03-11 13:12:58 -0600

    Add rust layer

--- init.el
+++ init.el
@@ -36,6 +36,7 @@ This function should only modify configuration layer settings."
      neotree
      org
      racket
+     rust
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
