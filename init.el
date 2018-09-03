22cffc32175236be03d1e2a6e175354cc69a5c5d
Devin Jacobs <dbuttonz@gmail.com>
2018-09-03 16:53:24 -0600

    Add spell-checking layer

--- init.el
+++ init.el
@@ -44,6 +44,7 @@ This function should only modify configuration layer settings."
      racket
      rust
      semantic
+     spell-checking
      version-control
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
@@ -55,7 +56,6 @@ This function should only modify configuration layer settings."
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
-     ;; spell-checking
      ;; syntax-checking
      )
    ;; List of additional packages that will be installed without being
